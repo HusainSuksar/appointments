e029536db6f55368e6709db6c61f8add9ddef15b
Husain Suksar <67893172+HusainSuksar@users.noreply.github.com>
2025-09-23 17:22:29 +0530

    Update savePatientPrescription.js

--- vercel-backend/api/savePatientPrescription.js
+++ vercel-backend/api/savePatientPrescription.js
@@ -1,14 +1,17 @@
-const db = require("../init");
+const cors = require('cors')({ origin: true });
+const db = require('../init');
 
 module.exports = async (req, res) => {
-  try {
-    const { id, prescription } = req.body;
-    if (!id) return res.status(400).json({ success: false, message: "ID required" });
+  cors(req, res, async () => {
+    try {
+      const { id, prescription } = req.body;
+      if (!id) return res.status(400).json({ success: false, message: "ID required" });
 
-    await db.collection("patients").doc(id).update({ prescription });
-    res.json({ success: true });
-  } catch (err) {
-    console.error(err);
-    res.json({ success: false, message: err.message });
-  }
+      await db.collection("patients").doc(id).update({ prescription });
+      res.json({ success: true });
+    } catch (err) {
+      console.error(err);
+      res.json({ success: false, message: err.message });
+    }
+  });
 };
