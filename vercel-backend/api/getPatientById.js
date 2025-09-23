4946c3a2e77ac07f5f4e8acc931151047a19f671
Husain Suksar <67893172+HusainSuksar@users.noreply.github.com>
2025-09-23 17:21:52 +0530

    Update getPatientById.js

--- vercel-backend/api/getPatientById.js
+++ vercel-backend/api/getPatientById.js
@@ -1,16 +1,19 @@
-const db = require("../init");
+const cors = require('cors')({ origin: true });
+const db = require('../init');
 
 module.exports = async (req, res) => {
-  try {
-    const { id } = req.query;
-    if (!id) return res.status(400).json({ error: "ID is required" });
+  cors(req, res, async () => {
+    try {
+      const { id } = req.query;
+      if (!id) return res.status(400).json({ error: "ID is required" });
 
-    const doc = await db.collection("patients").doc(id).get();
-    if (!doc.exists) return res.json(null);
+      const doc = await db.collection("patients").doc(id).get();
+      if (!doc.exists) return res.json(null);
 
-    res.json(doc.data());
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
+      res.json(doc.data());
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err.message });
+    }
+  });
 };
